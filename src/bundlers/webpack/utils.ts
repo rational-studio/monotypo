bc02ece79a729011f105314935712769f66ecc90
Zhongliang Wang <zwang@rebelworks.io>
2022-01-27 00:07:34 +0800

    integrate tailwind

--- src/bundlers/webpack/utils.ts
+++ src/bundlers/webpack/utils.ts
@@ -67,6 +67,7 @@ export function initWebpackConfig(
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>M</title>
+    <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body>
     <div id="app"></div>
